7fe1b0ce8b1a60c6d8e62f4f3dbd48596279fc55
Jonathan David Salcedo Diaz <79616003+Jonas4899@users.noreply.github.com>
2023-11-30 23:12:07 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2,9 +2,16 @@
 
 session_start();
 
+$DB_HOST = $_ENV["DB_HOST"]
+$DB_USER = $_ENV["DB_USER"] 
+$DB_PASSWORD = $_ENV["DB_PASSWORD"]
+$DB_NAME = $_ENV["DB_NAME"]
+$DB_PORT = $_ENV["DB_PORT"]
+
 $conn = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'renta_vehiculos'
+    "$DB_HOST",
+    "$DB_USER",
+    "$DB_PASSWORD",
+    "$DB_NAME",
+    "$DB_PORT"
 );
